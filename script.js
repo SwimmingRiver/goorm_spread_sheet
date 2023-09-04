49e5a26662996079399ded1276368762203c7335
swimmingRiver <swimmingr@gmail.com>
2023-09-04 12:47:41 +0900

    table 태그로 수정

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-let table = document.querySelector('#table');
-const column = ['R','A','B','C','D','E','F','G','H','I'];
-const row = ['O',1,2,3,4,5,6,7,8,9];
-let tableValue ;
+const columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+const rows = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+let table = '<table class="spreadsheet-table">';
 
-    for(let i=0;i<row.length;i++){
-        for(let j=0;j<column.length;j++){
-            const TableElement = document.createElement('input');
-            
-            TableElement.id = `${column[i]}${row[j]}`;
-            TableElement.className = 'tableElement';
-            TableElement.placeholder=`${TableElement.id}`;
-            
-            if(TableElement.placeholder.includes('R')){
-                TableElement.placeholder = column[j]
-                if(TableElement.placeholder==='O'){
-                    TableElement.placeholder=''
-                }
-            };
-            if(TableElement.placeholder.includes('O')){
-                TableElement.placeholder = row[i]  
-            }
-            if(TableElement.placeholder==='R'){
-                TableElement.placeholder='';
-                TableElement.disabled=true;
-            }
+table += '<tr><td></td>';
+for (let i = 0; i < columns.length; i++) {
+    table += `<td id='column${columns[i]}'>${columns[i]}</td>`;
+}
+table += '</tr>';
 
-            TableElement.addEventListener('focus',HighlightHandler);
-            table.appendChild(TableElement);
-        }
+for (let i = 0; i < rows.length; i++) {
+    table += '<tr>';
+    table += `<td id='row${rows[i]}'>${rows[i]}</td>`;
+    for (let j = 0; j < columns.length; j++) {
+        const cellId = `${columns[j]}${rows[i]}`;
+        table += `<td><input id="${cellId}" type="text" placeholder="${cellId}"></td>`;
     }
+    table += '</tr>';
+}
 
-    
-    function HighlightHandler(e){
-        const cellId = e.target.id;
-        const focusColumn = cellId.charAt(0);
-        const focusRow = cellId.slice(1);
+table += '</table>';
 
-        const cells = document.querySelectorAll('.tableElement');
-        cells.forEach(i => i.classList.remove('highlight'));
-    
+document.querySelector('#spreadSheet').innerHTML = table;
 
-        const rowCells = document.querySelectorAll(`input[id^='${focusColumn}'][id$='O']`);
-        rowCells.forEach(i => i.classList.add('highlight'));
-    
+const spreadSheetElement = document.querySelector('#spreadSheet');
 
-        const columnCells = document.querySelectorAll(`input[id^='R'][id$='${focusRow}']`);
-        columnCells.forEach(i => i.classList.add('highlight'));
-    }
-    
-    
-    
+
+const inputElements = spreadSheetElement.querySelectorAll('input[type="text"]');
+inputElements.forEach((inputElement) => {
+    inputElement.addEventListener('focus', HighlightHandler);
+});
+
+function HighlightHandler(e) {
+    const cellId = e.target.id;
+    const focusColumn = cellId.charAt(0);
+    const focusRow = cellId.slice(1);
+
+    const highlightedElements = document.querySelectorAll('.highlight');
+    highlightedElements.forEach(element => element.classList.remove('highlight'));
+
+
+    const firstRowCell = document.querySelector(`#row${focusRow}`);
+    firstRowCell.classList.add('highlight');
+
+    const firstColumnCell = document.querySelector(`#column${focusColumn}`);
+    firstColumnCell.classList.add('highlight');
+}
